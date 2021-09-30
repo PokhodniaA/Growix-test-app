8156c1383e1df23f3576483baf65b54b325af478
Eugene <eugene@totally.group>
2021-09-30 10:39:24 +0300

    added main-layout

---
+++ components/layouts/main/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-container fluid="lg">
+      <b-navbar-brand href="#">Growix</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#">Disabled</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
---
+++ layouts/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <b-container fluid="lg">
+      <Nuxt />
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '@/components/layouts/main/Header.vue'
+
+export default Vue.extend({
+  components: {
+    Header,
+  },
+})
+</script>
