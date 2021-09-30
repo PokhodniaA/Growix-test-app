80799ddf0b1e04a688085edbed1923ad01c48222
Eugene <eugene@totally.group>
2021-09-30 22:04:31 +0300

    updated Auth page

--- assets/lang/en.js
+++ assets/lang/en.js
@@ -7,5 +7,11 @@ export default {
         label: 'Email address',
         placeholder: 'Enter email',
         submit: 'Submit',
+        errors: {
+            required: 'Field is required',
+            minLength: 'Email must have at least 5 letters',
+            maxLength: 'Email must have at least 100 letters',
+            email: "Invalid email address.  Valid e-mail can contain only latin letters, numbers, '@' and '.'",
+        }
     }
 }
--- components/collections/Auth/Auth.vue
+++ components/collections/Auth/Auth.vue
@@ -7,21 +7,22 @@
         label-for="input-1"
       >
         <b-form-input
-          id="input-1"
-          v-model="email"
+          id="input-email"
+          v-model.trim="$v.email.$model"
           type="email"
-
-          aria-describedby="input-live-help input-live-feedback"
+          :state="showErrors"
+          aria-describedby="input-email-feedback"
           :placeholder="$t('form.placeholder')"
+          @input="checkErrors"
         ></b-form-input>
+
+        <!-- This will only be shown if the preceding input has an invalid state -->
+        <b-form-invalid-feedback id="input-email-feedback">{{
+          getValidationText
+        }}</b-form-invalid-feedback>
       </b-form-group>
 
-      <!-- This will only be shown if the preceding input has an invalid state -->
-      <b-form-invalid-feedback id="input-live-feedback">
-        Enter at least 3 letters
-      </b-form-invalid-feedback>
-
-      <b-button type="submit" variant="primary" :disabled="!activeButton">{{
+      <b-button type="submit" variant="primary" :disabled="!isValid">{{
         $t('form.submit')
       }}</b-button>
     </b-form>
@@ -31,20 +32,57 @@
 <script lang="ts">
 import Vue from 'vue'
 import FormWrapper from '@/components/ui/FormWrapper.vue'
+import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
+
+// const emailValidator: ValidationRule = function (email: any): Boolean {
+//   return true
+// }
 
 export default Vue.extend({
   components: { FormWrapper },
+  mixins: [validationMixin],
   data: () => ({
     email: '' as String,
+    showErrors: null as null | Boolean,
   }),
   computed: {
-    activeButton(): Boolean {
-      return this.email.length > 0
+    isValid(): Boolean {
+      this.$v.$touch()
+      return !this.$v.$invalid
+    },
+    getValidationText(): String {
+      if (!this.$v.email.required) {
+        // return this.$i18n.t('form.errors.required')
+      }
+
+      if (!this.$v.email.minLength) {
+        return `Email must have at least ${this.$v.email.$params.minLength.min} letters`
+      }
+
+      if (!this.$v.email.maxLength) {
+        return `Email must have at least ${this.$v.email.$params.maxLength.max} letters`
+      }
+
+      return "Invalid email address.  Valid e-mail can contain only latin letters, numbers, '@' and '.'"
     },
   },
   methods: {
     onSubmit(): void {
-      console.log('submit')
+      if (this.isValid) {
+        console.log('submit')
+      }
+    },
+    checkErrors(): void {
+      this.showErrors = this.isValid
+    },
+  },
+  validations: {
+    email: {
+      // emailValidator,
+      required,
+      minLength: minLength(5),
+      maxLength: maxLength(100),
     },
   },
 })
