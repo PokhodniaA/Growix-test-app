357c95a47fd7cdcd0049146a4bd5789d3d76a15c
Eugene <eugene@totally.group>
2021-09-30 13:57:00 +0300

    updated home page form

--- assets/lang/en.js
+++ assets/lang/en.js
@@ -2,5 +2,10 @@ export default {
     routeLinks: {
         home: 'Home',
         result: 'Result'
+    },
+    form: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        submit: 'Submit',
     }
 }
--- components/collections/Auth/Auth.vue
+++ components/collections/Auth/Auth.vue
@@ -1,21 +1,29 @@
 <template>
   <FormWrapper>
-    <b-form @submit="onSubmit">
+    <b-form @submit.prevent="onSubmit" novalidate>
       <b-form-group
         id="input-group-1"
-        label="Email address:"
+        :label="$t('form.label') + ':'"
         label-for="input-1"
       >
         <b-form-input
           id="input-1"
-          v-model="form.email"
+          v-model="email"
           type="email"
-          placeholder="Enter email"
-          required
+
+          aria-describedby="input-live-help input-live-feedback"
+          :placeholder="$t('form.placeholder')"
         ></b-form-input>
       </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <!-- This will only be shown if the preceding input has an invalid state -->
+      <b-form-invalid-feedback id="input-live-feedback">
+        Enter at least 3 letters
+      </b-form-invalid-feedback>
+
+      <b-button type="submit" variant="primary" :disabled="!activeButton">{{
+        $t('form.submit')
+      }}</b-button>
     </b-form>
   </FormWrapper>
 </template>
@@ -27,12 +35,15 @@ import FormWrapper from '@/components/ui/FormWrapper.vue'
 export default Vue.extend({
   components: { FormWrapper },
   data: () => ({
-    form: {
-      email: '',
-    },
+    email: '' as String,
   }),
+  computed: {
+    activeButton(): Boolean {
+      return this.email.length > 0
+    },
+  },
   methods: {
-    onSubmit() {
+    onSubmit(): void {
       console.log('submit')
     },
   },
